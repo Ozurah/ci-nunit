7736fd1d154738bbebcbe2bdde4dc088abfb2e94
Ozurah <80265286+Ozurah@users.noreply.github.com>
2025-08-20 13:56:57 +0200

    chore: Création d'un projet "App" et "Test"

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+
+namespace AppToTest
+{
+    public class App
+    {
+        public int CalcAdd(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using AppToTest;
+
+namespace Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestAdd_1()
+        {
+            int result = new App().CalcAdd(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void TestAdd_2()
+        {
+            int result = new App().CalcAdd(1, 2);
+            Assert.That(result, Is.EqualTo(666));
+        }
+    }
+}
